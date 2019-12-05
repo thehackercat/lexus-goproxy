1ecc2955fdaebeba8aaf5029c66a3d2545d05402
小虚大魔王 <lexuscyborg103@gmail.com>
2019-12-05 18:51:36 +0800

    feat: add dockerfile lint

---
+++ lint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+data=()
+
+echo ""
+echo "Searching for Dockerfiles ..."
+echo "-----------------------------"
+
+for f in $(find $GITHUB_WORKSPACE -name "Dockerfile*"); do
+    echo "Processing $f"
+    while IFS= read -r line; do
+      data+=( "$line" )
+    done < <( sh -c "hadolint $* $f" )
+done
+
+if [ ${#data[@]} -eq 0 ]; then
+    echo ""
+    echo "No vulnerabilities found!"
+    exit 0
+else
+    echo ""
+    echo "Found the following issues:"
+    echo "---------------------------"
+    printf '%s\n' "${data[@]}"
+    echo ""
+    echo "Look here for rules explanations: https://github.com/hadolint/hadolint#rules"
+    exit 1
+fi
